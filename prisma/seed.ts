196f96c4b3d7616c0722b1c3591995c978b634ee
Сергій Дидюк <s.dydiuk.3105@in.np.gov.ua>
2026-02-03 16:30:52 +0200

    init prisma setup

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,11 @@
 // prisma/seed.ts
 
-import { PrismaClient } from './generated/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import postgres from 'pg';
+import pg from 'pg';
 
 // Create a PostgreSQL adapter
-const pool = new postgres.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 // initialize Prisma Client
